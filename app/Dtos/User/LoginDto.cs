35382136093531b877604abb90767f1c7e152bba
Mostafijur Rahman <mostafijur1812@gmail.com>
2026-05-23 02:10:22 +0600

    feat: add validation attributes to LoginDto for email and password

--- app/Dtos/User/LoginDto.cs
+++ app/Dtos/User/LoginDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace app.Dtos.User
 {
     public class LoginDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100, MinimumLength = 6), RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$", ErrorMessage = "Password must be at least 6 characters long and include uppercase, lowercase, number and special character.")]
         public string Password { get; set; } = string.Empty;
     }
 }
